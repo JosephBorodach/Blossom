b900e6f95c58e18afdef68dba97a375af19d40e0
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2023-04-02 14:19:33 -0400

    Setup PartitionEqualSubsetSum

---
+++ DynamicProgramming/PartitionEqualSubsetSum/src/main/java/algo/practice/PartitionEqualSubsetSum.java
@@ -0,0 +1,32 @@
+package algo.practice;
+
+/**
+ * Approaches:
+ * 1. Brute Force
+ *      At every point there are two options
+ *      Compute every possibility
+ *
+ * 2.
+ *      a. Maintain 2 counts
+ *      b. Iterate over the array
+ *      c. add current num to pile which minimizes the difference
+ *      Or either if it doesn't make a difference
+ *
+ * Ex: [1,5,11,5]
+ *      c1 = 0
+ *      c2 = 0
+ *      1:
+ *          c1 = 1
+ *      5:
+ *          4 vs 6
+ *          c2 = 5
+ *
+ *
+ */
+public class PartitionEqualSubsetSum {
+    public boolean canPartition(int[] nums) {
+
+
+        return false;
+    }
+}
