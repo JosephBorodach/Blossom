8cd07d866d24e1766f7456b0765a0cd01b6639f6
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2023-04-14 10:44:20 -0400

    Completed ShortestUnsortedContinuousSubarray

---
+++ TwoPointers/ShortestUnsortedContinuousSubarray/src/main/java/algo/practice/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,66 @@
+package algo.practice;
+
+/*
+git add .
+
+git commit -m "Completed ShortestUnsortedContinuousSubarray"
+
+git push
+ */
+
+
+public class ShortestUnsortedContinuousSubarray {
+    /**
+     * @time: O(n), linear
+     *      To find unsorted section, and min & max is n exactly.
+     *      To expand outward will be at most n - 2
+     * @space: O(1), constant
+     */
+    public int findUnsortedSubarray(int[] nums) {
+        int len = nums.length;
+
+        int left = 1;
+        // Sorted from the left
+        while (left < len && nums[left - 1] <= nums[left]) {
+            left++;
+        }
+
+        // Already sorted
+        if (left == len) {
+            return 0;
+        }
+
+        int right = len - 1;
+        // Sorted from the right
+        while (right > 0 && nums[right] >= nums[right - 1]) {
+            right--;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = left; i <= right; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+
+        while (expandLeft(left, min, nums) || expandRight(right, max, nums)) {
+            while (expandLeft(left, min, nums)) {
+                max = Math.max(max, nums[left - 1]);
+                left--;
+            }
+            while (expandRight(right, max, nums)) {
+                min = Math.min(min, nums[right + 1]);
+                right++;
+            }
+        }
+        return right - left + 1;
+    }
+
+    private boolean expandLeft(int left, int min, int[] nums) {
+        return left > 0 && min < nums[left - 1];
+    }
+
+    private boolean expandRight(int right, int max, int[] nums) {
+        return right < nums.length - 1 && max > nums[right + 1];
+    }
+}
